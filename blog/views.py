70f955c59508b0e85a82c842bf1ffb4876492eb8
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-02 16:59:36 +0900

    Make Detail Page with CBV format

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-    {
-        'posts': posts,
-    }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
 # Create your views here.
