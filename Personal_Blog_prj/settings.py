9824687566eba11a41c1c1cd96b7641757b84297
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-18 21:34:09 +0800

    apply gunicorn to docker

--- Personal_Blog_prj/settings.py
+++ Personal_Blog_prj/settings.py
@@ -142,6 +142,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Personal_Blog_prj/urls.py
+++ Personal_Blog_prj/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 path('', include('single_pages.urls'))
 ]
+
