029ecee8158c8d1e055c063e281bd595f86741e3
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-21 10:42:04 +0800

    Apply letsencrypt

--- Personal_Blog_prj/settings.py
+++ Personal_Blog_prj/settings.py
@@ -28,7 +28,7 @@ DEBUG = int(os.environ.get('DEBUG', 1))
 if os.environ.get('DJANGO_ALLOWED_HOSTS'):
     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 else:
-    ALLOWED_HOSTS = ['13.250.154.127', '127.0.0.1', 'localhost']
+    ALLOWED_HOSTS = ['13.250.154.127', '127.0.0.1', 'localhost', 'minjoon-park-hk.com', 'www.minjoon-park-hk.com']
 
 
 # Application definition
