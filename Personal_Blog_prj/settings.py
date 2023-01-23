05537cf996e05f3db6869a24d1a09bbee9af1ac2
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-23 09:56:41 +0800

    Implementing image upload function

--- Personal_Blog_prj/settings.py
+++ Personal_Blog_prj/settings.py
@@ -144,6 +144,9 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField(null=True, blank=True, default='')
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
