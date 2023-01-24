add523a27f47c2969d564f0952273c5eee1030d4
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-24 20:22:31 +0800

    author name added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'content', 'head_image', 'file_upload']
 class PostList(ListView):
     model = Post
     template_name = 'blog/index.html'
