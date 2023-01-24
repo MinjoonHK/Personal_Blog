fc9e0ac598ea3636bbdd6d61b3733d587216192f
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-25 00:42:45 +0800

    UI updated

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('landing/', views.landing),
     path('about_me/', views.about_me),
     path('', views.landing),
     path('Contact/', views.Contact)
