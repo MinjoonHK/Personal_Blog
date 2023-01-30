4ef16642ba6ebb33da5b9be1223827646afc14f5
Minjoon Park <minjoon.park.hk@gmail.com>
2023-01-31 00:57:43 +0800

    Create, Edit, and Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,37 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
 
-class PostCreate(CreateView):
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content', 'head_image', 'file_upload']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'head_image', 'file_upload']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog')
 class PostList(ListView):
     model = Post
     template_name = 'blog/index.html'
     ordering = '-pk'
+    paginate_by = 1
 
 class PostDetail(DetailView):
     model = Post
